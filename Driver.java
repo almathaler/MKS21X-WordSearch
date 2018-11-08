80bdb6cf95618fdae543d2e2a678d1fe9d5e0202
almathaler <athaler00@stuy.edu>
2018-11-08 16:20:52 -0500

    did diagonal add

--- Driver.java
+++ Driver.java
@@ -13,14 +13,28 @@ public class Driver {
     fiveFour.addWordHorizontal("HEYA", 0, 0);
     System.out.println(fiveFour);
     /**/
-    System.out.println("Can we add HELLO from the first row second column?: (no, expect no change in fiveFour)");
+    System.out.println("Let's try adding HERA diagonally from the same H in the top left corner");
+    fiveFour.addWordDiagonal("HERA", 0, 0);
+    System.out.println(fiveFour);
+    /**/
+    System.out.println("Can we add HELLO vertically from the first row second column?: (no, expect no change in fiveFour)");
     fiveFour.addWordVertical("HELLO", 0, 1);
+    System.out.println(fiveFour);
+    /**/
+    System.out.println("Can we add HEYA from the first row second column?: (no, expect no change in fiveFour)");
+    fiveFour.addWordHorizontal("HEYA", 0, 1);
+    System.out.println(fiveFour);
+    /**/
+    System.out.println("Can we add HERA from the first row second column?: (no, expect no change in fiveFour)");
+    fiveFour.addWordDiagonal("HERA", 0, 1);
     System.out.println(fiveFour);
     /**/
     System.out.println("Adding words that are too long?: (expect no change)");
     fiveFour.addWordHorizontal("CATASTROPHE", 1, 1);
     fiveFour.addWordVertical("CATASTROPHE", 1, 1);
+    fiveFour.addWordDiagonal("CATASTROPHE", 1, 1);
     System.out.println(fiveFour);
+
 
   }
 }
